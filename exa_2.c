561ea4ef365c1f153c17520bfd3fb54c4c73d46d
amandrafino <amandrafino@gmail.com>
2023-10-24 14:20:18 -0400

    modifed exa_2" "

--- exa_2.c
+++ exa_2.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 
 // Function prototype declaration
-void stateAndCapital(char state[], char capital[]);
+void printStateAndCapital(char state[], char capital[]);
+void enterState(char state[]);
+void enterCapital(char capital[]);
 
-int main() {
-    char state[] = "Vermont";
-    char capital[] = "Montpelier";
-    stateAndCapital(state, capital);
-    
+
+int main(void) {
+    char state[50];
+    char capital[50];
+
+    enterState(state);
+    enterCapital(capital);
+
+    printStateAndCapital(state, capital);
+
     return 0;
 }
 
+
 // Function definition
-void stateAndCapital(char state[], char capital[]) {
-    printf("I live in the state of %s\n", state);
+void printStateAndCapital(char state[], char capital[]) {
+    printf("You live the the state of %s\n", state);
     printf("The capital of %s is %s\n", state, capital);
 }
+
+// Function definition
+void enterState(char state[]) {
+    printf("What state do you live in: ");
+    scanf("%[^\n]%*c", state);
+}
+
+// Function definition
+void enterCapital(char capital[]) {
+    printf("What is the capital of your state: ");
+    scanf("%[^\n]%*c", capital);
+}
+
